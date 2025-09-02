751040d33909bc560f4c92b75d3ed8e6a5df4ff2
YeniGoyes <ygoyes@unal.edu.co>
2025-09-01 19:52:08 -0500

    first commit

---
+++ yeny.py
@@ -0,0 +1,128 @@
+
+#Autor: Yeni Goyes#
+
+#Estructura de datos, ecuaciónes lógicas#
+
+#Definir el valor de A#
+
+A= float(input("Ingrese un número que esté entre el 1 y el 10, incluyéndolos: "))
+
+#Condicional de A#
+
+if(A>=1 or A<=10):
+    print("El valor es correcto")
+else: 
+    print("El valor es incorrecto2")
+
+print(type(A), A)
+
+#Definir el valor de B#
+
+B= float(input("Ingrese el número resultado de la ecuación 10+4: "))
+
+#Condicional de B#
+
+if(B==14):
+    print("El valor es correcto")
+else:
+    print("El valor es incorrecto")
+
+print(type(B),B)
+
+#Definir el valor de C#
+
+C= float(input("Ingrese un número menor o igual a 35: "))
+
+#Condicional de C#
+
+if(C<=35):
+    print("El valor indicado cumple la condición")
+else:
+    print("El valor indicado es incorrecto")
+
+print(type(C),C)
+
+#Definir el valor de D#
+
+D= float(input("Ingrese un número mayor o igual a 2: "))
+
+#Condicional de D#
+
+if(D>=2):
+    print("El valor indicado es correcto")
+else:
+    print("Valor incorrecto")
+
+print(type(D),D)
+
+#Definir el valor de E#
+
+E= float(input("indique un valor menor a 19: "))
+
+#Condicional de E#
+
+if(E<19):
+    print("El valor indicado es correcto: ")
+else:
+    print("El valor indicado es incorrecto")
+
+print(type(E),E)
+
+#Definir el valor de F#
+
+F= float(input("indíque un número que sea mayor a 32.4: "))
+
+#Condicional de F#
+
+if(F>32.4):
+    print("El dígito es correcto")
+else:
+    print("El dígito es incorrecto")
+
+print(type(F),F)
+
+#Definir el valor de H#
+
+H= float(input("Indíque números diferentes de 4: "))
+
+#Condicional de H#
+
+if(H!=4):
+    print("El número es cumple la condición")
+else:
+    print("El número es 4")
+
+print(type(H),H)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
